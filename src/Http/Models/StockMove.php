f42152a34ac2ec5db4591d26493e79849b94c7e8
sefirosweb <sefirosweb@gmail.com>
2025-06-19 15:35:29 +0200

    Added withTrashed

--- src/Http/Models/StockMove.php
+++ src/Http/Models/StockMove.php
@@ -32,6 +32,6 @@ class StockMove extends OdooModel
     public function product_product(): BelongsTo
     {
         $relation = config('laravel-odoo-connector.ProductProduct');
-        return $this->belongsTo($relation, 'product_id', 'id');
+        return $this->belongsTo($relation, 'product_id', 'id')->withTrashed();
     }
 }
